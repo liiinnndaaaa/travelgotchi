08a53230cc2c5b3cb1f81b3a90275aa2865235b2
Linda Krockert <linda.krockert@gmx.de>
2023-10-08 14:26:30 +0200

    Firebasedarstellung bearbeiten

--- Travelgotchi/app/src/main/java/com/example/travelgotchi/adapter/CountriesAdapter.kt
+++ Travelgotchi/app/src/main/java/com/example/travelgotchi/adapter/CountriesAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.travelgotchi.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
@@ -8,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.travelgotchi.R
 import com.example.travelgotchi.databinding.FragmentContinentsBinding
 import com.example.travelgotchi.model.CountryData
+import com.example.travelgotchi.view.NewActivity
 
 class CountriesAdapter(
     var c: Context, var countryList: ArrayList<CountryData>
@@ -27,6 +29,20 @@ class CountriesAdapter(
     }
 
     override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
+        val newList = countryList[position]
         holder.v.isCountries = countryList[position]
+        holder.v.root.setOnClickListener {
+            val img = newList.img
+            val name = newList.name
+            val info = newList.info
+
+            /** set Data */
+
+            val mIntent = Intent(c,NewActivity::class.java)
+            mIntent.putExtra("img", img)
+            mIntent.putExtra("name", name)
+            mIntent.putExtra("info", info)
+            c.startActivity(mIntent)
+        }
     }
 }
